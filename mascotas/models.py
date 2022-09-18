4f496210725c34beb9e0b08beeae6172ba39222f
MCrux43 <misael.ortega25@gmail.com>
2022-09-18 18:34:59 -0500

    imagefield añadido

--- mascotas/models.py
+++ mascotas/models.py
@@ -1,3 +1,4 @@
+from distutils.command.upload import upload
 from django.db import models
 
 class mascota (models.Model):
@@ -11,6 +12,7 @@ class mascota (models.Model):
         max_length=1,
         choices=opciones_sexo,
         default=Hembra)
+    foto = models.ImageField(upload_to = 'media/')
 
     def __str__(self):
         return self.especie, self.raza
