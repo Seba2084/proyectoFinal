1379e786adb044f949feb4d98c41cff90dd9d51c
MCrux43 <misael.ortega25@gmail.com>
2022-09-20 22:09:00 -0500

    idmascota

--- registrar_mascotas/models.py
+++ registrar_mascotas/models.py
@@ -1,15 +1,11 @@
 from django.db import models
-from mascotas.models import mascota
 
 class registrar (models.Model):
-    ##estas son para víncular las bd, pero aún no sé como lograrlo, si se descomentan no hace las migraciones##
-    #idmascot = mascota.especie 
-    #idpet = ([idmascot,"mascota.especie"])
-    #pet = models.CharField(max_length=20 ,choices=idmascot)
+    id_mascota = models.CharField(max_length=5) 
     encontrador = models.CharField(max_length=20)
     fecha_encontrado = models.DateField()
     email = models.EmailField()
     lugar_encontrado = models.CharField(max_length=50)
 
     def __str__(self):
-        return self.mascot, self.encontrador, self.email
+        return self.id_mascot, self.encontrador, self.email
